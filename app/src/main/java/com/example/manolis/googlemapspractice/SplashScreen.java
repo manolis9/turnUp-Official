d56d19d62fe52955573592e9e634c20bbbe77e16
EMMANUEL IOANNIDES <manolis@alumni.ubc.ca>
2015-09-04 18:04:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/manolis/googlemapspractice/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.manolis.googlemapspractice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Manolis on 24/08/2015.
+ */
+public class SplashScreen extends Activity{
+
+    private static int SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+}
